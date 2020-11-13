25f94b582e0badac8dc7d975a702123f36ff11df
yangyang <1434753182@qq.com>
2020-11-13 15:45:12 +0800

    首页-获取分类数据

--- src/views/Home/category.vue
+++ src/views/Home/category.vue
@@ -1,26 +1,46 @@
 <template>
   <div>
     <ul class="cate-box">
-      <li class="cate-list" v-for="(item,index) in 10" :key="index">
-        <img
-          class="cate-img"
-          src="http://admin.gxxmglzx.com/tender/upload/test/type1.png"
-        />
-        <p>美食</p>
+      <li class="cate-list" v-for="(item, index) in cateList" :key="item.id">
+        <img class="cate-img" :src="item.img" />
+        <p>{{ item.name }}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cateList: [],
+    };
+  },
+  created() {
+    axios({
+      url: "http://admin.gxxmglzx.com/tender/test/get_type",
+    })
+      .then((res) => {
+        if (res.data.errcode === 200) {
+          console.log("获取分类数据", res.data);
+          this.cateList = res.data.data;
+        } else {
+          console.log(res.data.errmsg);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .cate-box {
   display: flex;
   flex-wrap: wrap;
-  
+
   .cate-list {
     width: 20%;
     text-align: center;
